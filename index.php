cdaa621e54a6f985e28048706e7c9fc7417df790
alekseypuzyrin <puzyrin7@mail.ru>
2018-10-01 21:11:19 +0300

    les4

---
+++ index.php
@@ -0,0 +1,57 @@
+<!--//сделать ф-ю которая будет обрезать текст до определенного символа-->
+<?php
+//   $text="dlkflk lkl p hello world! hello world! ";
+//   function cut_text($text,$num_letters){
+//       $num =strlen($text);
+//       if ($num>$num_letters){
+//           $text=substr($text, 0,$num_letters);
+//           $text.="...";
+//       }
+//       return $text;
+//   }
+//   echo cut_text($text, 10);
+//?>
+<!--1. ‘оздать функцию, которая возвращает сумму двух чисел;-->
+<?php
+//    function FunSum($num1, $num2){
+//        $sum=$num1+$num2;
+//        return $sum;
+//    }
+//?>
+<!--2. ‘оздать функцию, которая выводит на экран сумму двух чисел;-->
+<?php
+//    function FunSum2($num1, $num2){
+//        $sum=$num1+$num2;
+//       return $sum;
+//}
+//echo FunSum2(1,3)
+//?>
+<!--3. ‘оздать функцию приветствия;-->
+<?php
+//    function hello($a){
+//    return $a.", hellooo";
+//}
+//    echo hello("aleksey");
+//?>
+<!--5. ”ункциЯ, принимающаЯ массив строк и выводЯщаЯ каждую строку в отдельном К К К КК К К КК-->
+<!--параграфе.-->
+<?php
+//    $arr=array( 'K', 'K', 'KK', 'K', 'K', 'KK' );
+//    array_walk($arr, function($exit){
+//        echo $exit."<br>";
+//    }
+//    );
+//?>
+<!--4. создать функцию, которая отрисовывает таблицу умножения;дорешать-->
+<?php
+//    $a=5;
+//    $b=5;
+//
+//    for ($i=1;$a>=$i;$i++){
+//        echo "tr";
+//        for($j=0,$l=0;$b>$l;l++,j+=$i){
+//            echo "<td".($i+$j)."</td">;
+//        }
+//    }
+//?>
+
